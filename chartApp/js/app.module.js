cd1aad411e808a4993ce805a13794c37fed977c1
Adwaitya Sadhukhan <asadhukhan@dunnsolutions.com>
2016-07-05 10:57:57 +0530

    initial commit

--- chartApp/js/app.module.js
+++ chartApp/js/app.module.js
@@ -2,21 +2,8 @@
 (function(){
 	'use strict';
 
-	angular.module('chartApp',["chart.js"])
-			.controller("ChartController",ChartController);
+	angular.module('chartApp',["chart.js"]);
+			
 
-			function ChartController(){
-				var vm = this;
-				vm.title ="Chart App";
-
-				 vm.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    			 vm.series = ['Series A', 'Series B'];
-                vm.data  = [
-                             [65, 59, 80, 81, 56, 55, 40],
-                             [28, 48, 40, 19, 86, 27, 90]
-                        ];
-                   vm.onClick = function (points, evt) {
-                     console.log(points, evt);
-                         };
-			}
+			
 })();
